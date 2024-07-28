08a1b2d0187815632cb27a6303fa9ac4f9effc0b
millenium-1 <louiskaroki2010@gmail.com>
2024-07-28 20:30:29 +0300

    next commit

--- todoapp/todo_app/models.py
+++ todoapp/todo_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class person(models.Model):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    birth_date = models.DateField()
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    status = models.BooleanField(default=False, verbose_name="completed")
+    created_at = models.DateField(auto_now_add=True)
+    due_date = models.DateField(auto_now=True)
 
     def __str__(self):
-        return f"{self.pk}. {self.first_name} {self.last_name}"
-    # self.pk means primary key eg 1 
+        return f"title: {self.title}\nstatus{self.status}"
