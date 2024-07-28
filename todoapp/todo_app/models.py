fa32b6280eac0371cba1a4d3cbe70638b82dd5ce
millenium-1 <louiskaroki2010@gmail.com>
2024-07-28 17:07:32 +0300

    next commit

--- todoapp/todo_app/models.py
+++ todoapp/todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class person(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.pk}. {self.first_name} {self.last_name}"
+    # self.pk means primary key eg 1 
