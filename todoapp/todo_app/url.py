7d5d95d4f459692f2b836a85f52d17b2291e692d
millenium-1 <louiskaroki2010@gmail.com>
2024-08-29 17:46:50 +0300

    next commit

--- todoapp/todo_app/url.py
+++ todoapp/todo_app/url.py
@@ -2,14 +2,14 @@
 from operator import index # not a must
 from django.urls import path
 # importing the view you want to map fro the url
-from .views import hello, index, task_list, task_detail, task_create, task_update, task_delete
+from .views import hello, index, TaskListView, TaskDetailView, task_create, task_update, task_delete
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
-    path('', task_list, name='list'),
-    path('detail/<int:id>/', task_detail, name='detail'),# we are passing an argument
+    path('', TaskListView.as_view(), name='list'),
+    path('detail/<int:pk>', TaskDetailView.as_view(), name='detail'),# we are passing an argument
     path('update/<int:id>/', task_update, name='update'),# we are passing an argument
     path('delete/<int:id>/', task_delete, name='delete'),
     path('create/', task_create, name='create'),
--- todoapp/todo_app/views.py
+++ todoapp/todo_app/views.py
@@ -1,9 +1,11 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskCreateForm
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.contrib.auth.decorators import login_required 
+from django.views.generic import View, DetailView, ListView
 
 # logindecorator is used to allow specific users to access specific documents
 
@@ -22,25 +24,43 @@ def index(request):
     return render(request, 'todo_app/index.html', context)
 
 # taking request from models for users to viewers to use it
-def task_list(request):
-    tasks = Task.objects.all()
-    paginator = Paginator(tasks, 2, orphans=1, allow_empty_first_page=True)# orphans are used to increase the numbers of tasks in the last page
-    page = request.GET.get('page')
 
-    try:
-        paginated_tasks = paginator.page(page)
-    except PageNotAnInteger:
-    # If page is not an integer, deliver the first page.
-        paginated_tasks = paginator.page(1)
-    except EmptyPage:
-    # If page is out of range (e.g., 9999), deliver last page of results.
-        paginated_tasks = paginated_tasks.page(paginator.num_pages)
-    return render(request, 'todo_app/list.html', {'tasks':paginated_tasks})
+# enables better handling of http requSests
+class TaskListView(View):
+    template_name = 'todo_app/list.html'
 
-def task_detail(request, id):
+    def get(self, request):
+        tasks = Task.objects.all()
+        paginator = Paginator(tasks, 2, orphans=1, allow_empty_first_page=True)# orphans are used to increase the numbers of tasks in the last page
+        page = request.GET.get('page')
+
+        try:
+            paginated_tasks = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver the first page.
+            paginated_tasks = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g., 9999), deliver last page of results.
+            paginated_tasks = paginated_tasks.page(paginator.num_pages)
+
+            context = {'tasks':paginated_tasks, 'title':'list tasks'}
+
+        return render(request, self.template_name, context)
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'todo_app/detail.html'
+    context_object_name = 'task'
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Task'
+        return context
+
+"""def task_detail(request, id):
     tasks = get_object_or_404(Task, id=id) # we are passing an argument
     return render(request, 'todo_app/detail.html', {'task':tasks})
-
+"""
 
 from django.shortcuts import render, redirect
 from .forms import TaskCreateForm
@@ -80,3 +100,5 @@ def task_delete(request, id):
         return redirect('list')
     
     return render(request, 'todo_app/delete.html', {'task':task})
+
+
